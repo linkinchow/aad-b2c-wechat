561ee2f32ca95254cb85d344b14147f553f59112
linkinchow <176547141@qq.com>
2018-08-30 13:40:36 +0800

    Update Constant.java

--- mobile/WeixinTest-master/app/src/main/java/com/ansen/shoenet/utils/Constant.java
+++ mobile/WeixinTest-master/app/src/main/java/com/ansen/shoenet/utils/Constant.java
@@ -5,8 +5,8 @@ package com.ansen.shoenet.utils;
  * @create time 2017-09-14
  */
 public class Constant {
-    public static String WECHAT_APPID = "wx4660d1e7374b75b2";
-    public static String WECHAT_SECRET = "59c93a4f13c528db83906f53f0f64de6";
+    public static String WECHAT_APPID = "";
+    public static String WECHAT_SECRET = "";
     //public static String B2C_REST_API = "https://login.microsoftonline.com/maxtenant.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1A_signup_signin_WC_MFA_Mobile&client_id=0b88f517-a8c4-4ae8-8abf-fe32bf336f0e&nonce=defaultNonce&redirect_uri=https%3A%2F%2Fwww.baidu.com&scope=openid&response_type=id_token&prompt=login&ui_locales=zh-hans";
     public static String B2C_REST_API = "https://login.microsoftonline.com/maxtenant.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1A_signup_signin_WC_MFA_Mobile&client_id=1c57e6ff-f624-4a8c-a359-9e42825e88bd&nonce=defaultNonce&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&scope=1c57e6ff-f624-4a8c-a359-9e42825e88bd%20offline_access&response_type=code&response_mode=query&prompt=login&ui_locales=zh-hans";
 }
